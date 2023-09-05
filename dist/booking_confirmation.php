d8bc4054d4366b752414d149d814552724185277
fil-design <fildesign6@gmail.com>
2023-09-05 12:08:17 +0200

    vendor php mailer

--- dist/booking_confirmation.php
+++ dist/booking_confirmation.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+// Include PHPMailer autoloader
+require '../vendor/autoload.php';
+
 // Function to extract name and email from query parameters
 function extractInfoFromQueryParameters($queryParameters) {
     parse_str($queryParameters, $params);
@@ -15,85 +21,57 @@ if (isset($_SERVER['QUERY_STRING'])) {
     $info = extractInfoFromQueryParameters($_SERVER['QUERY_STRING']);
 
     if ($info['name'] && $info['email']) {
-        $invoiceData = [
-            'logo' => 'http://invoiced.com/img/logo-invoice.png',
-            'from' => 'Invoiced\n701 Brazos St\nAustin, TX 78748',
-            'to' => $info['name'],
-            'currency' => 'usd',
-            'number' => 'INV-0001',
-            'payment_terms' => 'Auto-Billed - Do Not Pay',
-            'items' => [
-                [
-                    'name' => 'Subscription to Starter',
-                    'quantity' => 1,
-                    'unit_cost' => 50,
+        // Create a PHPMailer instance
+        $mail = new PHPMailer(true);
+
+        try {
+            // Server settings
+            $mail->SMTPDebug = 0;   // Enable verbose debug output (change to 2 for debugging)
+            $mail->isSMTP();
+            $mail->Host = 'sandbox.smtp.mailtrap.io';  // Specify your SMTP server
+            $mail->SMTPAuth = true;
+            $mail->Username = '8239cdb6d51b88'; // SMTP username
+            $mail->Password = '3c674995f83d25';   // SMTP password
+            $mail->SMTPSecure = 'tls';   // Enable TLS encryption
+            $mail->Port = 587;   // TCP port to connect to
+
+            // Sender and recipient information
+            $mail->setFrom('your@example.com', 'Your Name');
+            $mail->addAddress($info['email'], $info['name']);
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Invoice for Your Purchase';
+            // Generate your invoice data here and include it in the email body
+            $invoiceData = [
+                'logo' => 'http://invoiced.com/img/logo-invoice.png',
+                'from' => 'Invoiced\n701 Brazos St\nAustin, TX 78748',
+                'to' => $info['name'],
+                'currency' => 'usd',
+                'number' => 'INV-0001',
+                'payment_terms' => 'Auto-Billed - Do Not Pay',
+                'items' => [
+                    [
+                        'name' => 'Subscription to Starter',
+                        'quantity' => 1,
+                        'unit_cost' => 50,
+                    ],
                 ],
-            ],
-            'fields' => [
-                'tax' => '%',
-            ],
-            'tax' => 5,
-            'notes' => 'Thanks for being an awesome customer!',
-            'terms' => 'No need to submit payment. You will be auto-billed for this invoice.',
-        ];
+                'fields' => [
+                    'tax' => '%',
+                ],
+                'tax' => 5,
+                'notes' => 'Thanks for being an awesome customer!',
+                'terms' => 'No need to submit payment. You will be auto-billed for this invoice.',
+            ];
+            $mail->Body = 'Here is your invoice:<br>' . json_encode($invoiceData, JSON_PRETTY_PRINT);
 
-        // Convert the invoice data to JSON
-        $invoiceJson = json_encode($invoiceData);
-
-        // Generate the JavaScript code to download the invoice PDF
-        $jsCode = <<<EOD
-var https = require("https");
-var fs = require("fs");
-
-function generateInvoice(invoice, filename, success, error) {
-    var postData = JSON.stringify(invoice);
-    var options = {
-        hostname  : "invoice-generator.com",
-        port      : 443,
-        path      : "/",
-        method    : "POST",
-        headers   : {
-            "Content-Type": "application/json",
-            "Content-Length": Buffer.byteLength(postData)
+            // Send the email
+            $mail->send();
+            echo 'Invoice email sent successfully.';
+        } catch (Exception $e) {
+            echo 'Email could not be sent. Error: ', $mail->ErrorInfo;
         }
-    };
-
-    var file = fs.createWriteStream(filename);
-
-    var req = https.request(options, function(res) {
-        res.on('data', function(chunk) {
-            file.write(chunk);
-        })
-        .on('end', function() {
-            file.end();
-
-            if (typeof success === 'function') {
-                success();
-            }
-        });
-    });
-    req.write(postData);
-    req.end();
-
-    if (typeof error === 'function') {
-        req.on('error', error);
-    }
-}
-
-var invoice = $invoiceJson;
-
-generateInvoice(invoice, 'invoice.pdf', function() {
-    console.log("Saved invoice to invoice.pdf");
-}, function(error) {
-    console.error(error);
-});
-EOD;
-
-        // Output the JavaScript code
-        echo $jsCode;
-
-        // Display a confirmation message
-        echo '<h1>Thank you for your submission, ' . htmlspecialchars($info['name']) . '!</h1>';
     } else {
         echo 'Name and email not found in query parameters.' . PHP_EOL;
     }
@@ -101,15 +79,3 @@ EOD;
     echo 'No query parameters found in the URL.' . PHP_EOL;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Confirmation Page</title>
-</head>
-<body>
-    <h1>THANK YOU FOR THE ORDER</h1>
-</body>
-</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '779f30605e9150eb7a1e7717633395621f61868b',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '779f30605e9150eb7a1e7717633395621f61868b',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.1',
+            'version' => '6.8.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+            'dev_requirement' => false,
+        ),
+    ),
+);
